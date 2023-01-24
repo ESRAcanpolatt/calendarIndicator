28b557c149dd453f2cbe98dd5b643f0a2f6c844c
Esra Canpolat <esracanpolat1997@gmail.com>
2023-01-24 12:31:12 +0300

    calenderIndicator

---
+++ pythonProject/main.py
@@ -0,0 +1,11 @@
+import calendar
+print("""************************************
+        Calendar Indicator
+*******************************""")
+month=int(input("Mont:"))
+year=int(input("Year:"))
+print(calendar.month(year,month))
+
+
+
+
